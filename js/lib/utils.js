f846a01b1c19708f83e9924674614134c2077d44
Yaroslav Konoplov <eahome00@gmail.com>
2017-07-02 13:04:14 +0300

    Add app.utils.escapeHTML

--- js/lib/utils.js
+++ js/lib/utils.js
@@ -1,1 +1,18 @@
-app.utils = {};
+app.utils = {
+
+  escapeHTML: (function() {
+    var rules = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;'
+    };
+
+    function replacement(c) { return rules[c] != null ? rules[c] : c }
+
+    return function(html) {
+      return (typeof html === 'string' ? html : html + '').replace(/[&<>"]/g, replacement)
+    }
+  })()
+
+};
