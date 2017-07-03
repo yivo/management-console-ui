23acf327b5c004c463d7a9ba836de97eec937754
Yaroslav Konoplov <eahome00@gmail.com>
2017-07-03 13:00:33 +0300

    Connect console and status

--- js/app/status.js
+++ js/app/status.js
@@ -11,5 +11,11 @@
     var el = document.getElementById('console-status');
     el.classList.add('hide');
   }
-  
+
+  app.on('did-load-console', showStatus);
+
+  app.on('will-load-console', hideStatus);
+
+  app.on('did-fail-load-console', hideStatus);
+
 })();
